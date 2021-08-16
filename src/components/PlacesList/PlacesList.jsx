df7c8b0a4bae5eb143ff2c6f5c23a97190514a5e
Neenus Gabriel <hello@neenus.com>
2021-08-16 00:58:05 -0400

    feat(placeslist.jsx): add loading and error status and feedback when fetching data

--- src/components/PlacesList/PlacesList.jsx
+++ src/components/PlacesList/PlacesList.jsx
@@ -1,10 +1,34 @@
-import { Grid } from "@material-ui/core";
+import {
+  Grid,
+  Backdrop,
+  CircularProgress,
+  Snackbar,
+  makeStyles
+} from "@material-ui/core";
+import MuiAlert from "@material-ui/lab/Alert";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import PlaceCard from "../PlaceCard/PlaceCard";
 
+const useStyles = makeStyles(theme => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff"
+  },
+  snackbar: {
+    backgroundColor: theme.MuiSnackbarContent
+  }
+}));
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
 const PlacesList = () => {
-  const [places, setPlaces] = useState([]);
+  const classes = useStyles();
+  const [places, setPlaces] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const API_BASE_URL =
     "https://610bb7502b6add0017cb3a35.mockapi.io/api/v1/places";
@@ -13,8 +37,11 @@ const PlacesList = () => {
       try {
         const places = await axios.get(API_BASE_URL);
         setPlaces(places.data);
-      } catch (error) {
-        console.log(error);
+        setIsLoading(false);
+        setError(null);
+      } catch (err) {
+        setIsLoading(false);
+        err.message ? setError(err.message) : setError(null);
       }
     };
     getPlaces();
@@ -22,12 +49,23 @@ const PlacesList = () => {
   return (
     <div>
       <Grid container spacing={3}>
-        {places.map(place => (
-          <Grid item key={place.id} xs={12}>
-            <PlaceCard place={place} />
-          </Grid>
-        ))}
+        {isLoading && (
+          <Backdrop className={classes.backdrop} open={isLoading}>
+            <CircularProgress />
+          </Backdrop>
+        )}
+        {places &&
+          places.map(place => (
+            <Grid item key={place.id} xs={12}>
+              <PlaceCard place={place} />
+            </Grid>
+          ))}
       </Grid>
+      {error && (
+        <Snackbar open={error} autoHideDuration={6000}>
+          <Alert severity="error">{error}</Alert>
+        </Snackbar>
+      )}
     </div>
   );
 };
